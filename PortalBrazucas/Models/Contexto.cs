8256572a9c56bbd7455ab76562b97fe8e1f3e538
Wagner06 <wgs06@msn.com>
2013-03-27 23:08:45 -0300

    Comitando todo o projeto

---
+++ PortalBrazucas/Models/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace PortalBrazucas.Models
+{
+    public class Contexto:DbContext
+    {
+        public Contexto(): base("Qualquer"){}
+
+        public DbSet<Pais> Paises { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ PortalBrazucas/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalBrazucas.Models
+{
+    public abstract class Usuario
+    {
+        public int Id { get; set; }
+        public string nome { get; set; }
+
+    }
+}
